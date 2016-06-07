58f0579db852f5a93fbae323b62e9d9748a699a3
Olaf Andreas Øvrum <olaf@huconglobal.com>
2016-06-07 15:07:04 +0200

    Initial Commit

---
+++ SimpleNumpad/SimpleNumpad.h
@@ -0,0 +1,19 @@
+//
+//  SimpleNumpad.h
+//  SimpleNumpad
+//
+//  Created by Olaf Øvrum on 07.06.2016.
+//  Copyright © 2016 Viking Software Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleNumpad.
+FOUNDATION_EXPORT double SimpleNumpadVersionNumber;
+
+//! Project version string for SimpleNumpad.
+FOUNDATION_EXPORT const unsigned char SimpleNumpadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleNumpad/PublicHeader.h>
+
+
